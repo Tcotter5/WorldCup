7cbf4f90c6afc9194e4af7400f927acb30edf82d
Tim Cotter <100786270+Tcotter5@users.noreply.github.com>
2022-11-17 13:39:55 -0500

    Add files via upload
    adjusted my approach

--- worldcup.py
+++ worldcup.py
@@ -1,7 +1,6 @@
 # pip install requests
 # pip install beautifulsoup4
 # pip install pretty printer (get a better idea of where to call on html file upon first scrape)
-import pprint
 # pip install panadas
 # pip install lxml
 # pip install html5lib
@@ -12,32 +11,42 @@ import pandas as pd
 
 url = "https://fbref.com/en/comps/1/schedule/World-Cup-Scores-and-Fixtures"
 
+# applying url to variable results via requests
 result = requests.get(url)
+
 doc = BeautifulSoup(result.text, "html.parser")
 
+# pass html (result.text) /  find specific string using match
+matches = pd.read_html(result.text, match="Scores & Fixtures")
+
+# wanted to see all available columns, only showing 4 beforehand
+pd.options.display.max_columns = 14
+
+print(matches[0].head(0))
+
+
+
+# ***** keeping old code commented out, not what I wanted to do but good to have for reference *******
 # find table via inspect tool / search on first instance of class name stats_table using 0
-table = doc.select('table.stats_table')[0]
+# table = doc.select('table.stats_table')[0]
 
 # find all instances of a within html file
-links = table.find_all('a')
+# links = table.find_all('a')
 
 # grab hrefs
-links = [l.get("href") for l in links]
+# links = [l.get("href") for l in links]
 
 # filter links to only get squad links via list comprehension, "is squad in link/ if not do not retrieve"
-links = [l for l in links if '/squads/' in l]
+# links = [l for l in links if '/squads/' in l]
 
 # add additional https: to partial urls from before
-squad_urls = [f"https://fbref.com{l}" for l in links]
+# squad_urls = [f"https://fbref.com{l}" for l in links]
 
 # get first link
-squad_urls = squad_urls[0]
+# squad_urls = squad_urls[0]
 
-result = requests.get(squad_urls)
+# matches = pd.read_html(result.text, match="Home & Away")
 
-matches = pd.read_html(result.text, match="Home & Away")
-
-print(matches)
 
 
 
